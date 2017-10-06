59a68a04192d5fcfb5cad818b54e6d09d463231d
ebeedle <ebeedle@ucla.edu>
2017-10-05 21:57:34 -0700

    added xorgin request headers

--- server/index.js
+++ server/index.js
@@ -15,6 +15,11 @@ var utils = require('./lib/hashUtils.js')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(function (req, res, next) {
     res.setHeader('Cache-Control', 'public, max-age=5')
